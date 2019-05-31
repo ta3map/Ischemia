c27bf9dbfbc9b5e4e47d42d6e675f61645f73036
Neuro-PC\NeuroLab <ta3map@gmail.com>
2019-05-31 17:46:40 +0300

    mind map about Ischemia

---
+++ analysis/Ischemia.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1559298839184" ID="ID_1486697768" MODIFIED="1559306589580" TEXT="Ischemia">
+<node CREATED="1559306387503" ID="ID_755472395" LINK="file:/D:/Neurolab/ialdev/Ischemia/analysis/OIS_analysis.m" MODIFIED="1559306387503" POSITION="right" TEXT="OIS_analysis.m">
+<node CREATED="1559306395375" ID="ID_1454709857" LINK="file:/D:/Neurolab/ialdev/Ischemia/analysis/lfp_make_lfp.m" MODIFIED="1559306395375" TEXT="lfp_make_lfp.m">
+<node CREATED="1559306505352" LINK="file:/D:/Neurolab/Data/Ischemia/Traces/lfp_image/496_lfp_image_310519_P17_slc2.jpg" MODIFIED="1559306505352" TEXT="496_lfp_image_310519_P17_slc2.jpg"/>
+</node>
+<node CREATED="1559306401607" ID="ID_883583026" LINK="file:/D:/Neurolab/ialdev/Ischemia/analysis/ois_make_ois.m" MODIFIED="1559306401607" TEXT="ois_make_ois.m">
+<node CREATED="1559306524872" LINK="file:/D:/Neurolab/Data/Ischemia/Traces/ios_video/496_ios_video_310519_P17_slc2.mp4" MODIFIED="1559306524872" TEXT="496_ios_video_310519_P17_slc2.mp4"/>
+</node>
+<node CREATED="1559306404520" ID="ID_36420592" LINK="file:/D:/Neurolab/ialdev/Ischemia/analysis/ois_plot_ois.m" MODIFIED="1559306404520" TEXT="ois_plot_ois.m">
+<node CREATED="1559306495456" LINK="file:/D:/Neurolab/Data/Ischemia/Traces/ios_image/496_ios_image_310519_P17_slc2.jpg" MODIFIED="1559306495456" TEXT="496_ios_image_310519_P17_slc2.jpg"/>
+</node>
+<node CREATED="1559306427246" ID="ID_1263233512" LINK="file:/D:/Neurolab/ialdev/Ischemia/analysis/lfp_and_OIS_plot.m" MODIFIED="1559306427246" TEXT="lfp_and_OIS_plot.m">
+<node CREATED="1559306535792" ID="ID_466604717" LINK="file:/D:/Neurolab/Data/Ischemia/Traces/lfp_and_OIS_image/496_lfp_and_OIS_image_310519_P17_slc2.jpg" MODIFIED="1559306535792" TEXT="496_lfp_and_OIS_image_310519_P17_slc2.jpg"/>
+</node>
+</node>
+<node CREATED="1559306448151" ID="ID_1856638634" LINK="file:/D:/Neurolab/ialdev/Ischemia/Protocol/IschemiaProtocol.xlsx" MODIFIED="1559306448151" POSITION="left" TEXT="IschemiaProtocol.xlsx"/>
+</node>
+</map>
